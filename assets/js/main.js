4f64e48c7d1c925aeec6ad0e43a94c21bc6cf716
ADV Indian coder <116486037+ADVindiancoder@users.noreply.github.com>
2023-08-27 14:07:46 -0700

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+(function() {
+    
+    "use strict";
+    
+    //===== Prealoder
+
+    window.onload = function() {
+        window.setTimeout(fadeout, 100);
+    }
+
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
+
+    
+    /*=====================================
+    Sticky
+    ======================================= */
+    window.onscroll = function () {
+        var header_navbar = document.getElementById("header_navbar");
+        var sticky = header_navbar.offsetTop;
+
+        if (window.pageYOffset > sticky) {
+            header_navbar.classList.add("sticky");
+        } else {
+            header_navbar.classList.remove("sticky");
+        }
+
+
+
+        // show or hide the back-top-top button
+        var backToTo = document.querySelector(".back-to-top");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "block";
+        } else {
+            backToTo.style.display = "none";
+        }
+    };
+
+
+        //===== close navbar-collapse when a  clicked
+    let navbarToggler = document.querySelector(".navbar-toggler");    
+    var navbarCollapse = document.querySelector(".navbar-collapse");
+
+    document.querySelectorAll(".page-scroll").forEach(e =>
+        e.addEventListener("click", () => {
+            navbarToggler.classList.remove("active");
+            navbarCollapse.classList.remove('show')
+        })
+    );
+    navbarToggler.addEventListener('click', function() {
+        navbarToggler.classList.toggle("active");
+    });
+    
+
+    //WOW Scroll Spy
+    var wow = new WOW({
+        //disabled for mobile
+        mobile: false
+    });
+    wow.init();
+
+
+
+})();
